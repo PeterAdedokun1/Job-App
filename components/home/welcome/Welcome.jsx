cc71287eb1902408fd8560a8958800d326bd6e6e
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-17 14:06:49 +0100

    fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import {Text,View} from "react-native"
+import { Text, View, TextInput, TouchableOpacity, Image, FlatList } from "react-native"
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
+import styles from "./welcome.style"
+import  {icons,SIZES} from "../../../constants"
 const Welcome = () => {
+  const router = useRouter()
   return (
-    <Text>Welcome</Text>
-  )
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian </Text>
+        <Text style={styles.welcomeMessage}>Find your perfect Job </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=''
+            onChange={() => { }}
+            placeholder='What are you looking for?'
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 export default Welcome
