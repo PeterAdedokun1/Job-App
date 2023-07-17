8813724f81721ab649b45551f557549bd69ba716
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-17 23:22:26 +0100

    job details start

---
+++ app/job-details/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from "react-native"
+const JobDetails = () => {
+  return (
+      <View>
+          <Text>jsjsj</Text>
+  </View>
+  )
+}
+
+export default JobDetails;
