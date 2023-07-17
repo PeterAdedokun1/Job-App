634506e28fc254ee7c6032f77ad0994ce6d6d67b
peterAdedokun1 <peteradedokun436@gmail.com>
2023-07-17 16:33:52 +0100

    welcome page

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -3,9 +3,14 @@ import { Text, View, TextInput, TouchableOpacity, Image, FlatList } from "react-
 import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import styles from "./welcome.style"
-import  {icons,SIZES} from "../../../constants"
+import { icons, SIZES } from "../../../constants"
+
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 const Welcome = () => {
   const router = useRouter()
+  // const [search,sear]
+  const [activeJobType, setActiveJobType] = useState("Full-time")
   return (
     <View>
       <View style={styles.container}>
@@ -16,11 +21,36 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=''
-            onChange={() => { }}
-            placeholder='What are you looking for?'
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
           />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item }</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item.id}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
